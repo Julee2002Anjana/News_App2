a53ed16032d6d45b3641a6d725bb9a268fc02f69
Julee Anjana <julieanjana2002@gmail.com>
2025-06-11 00:30:14 +0530

    firebasc connect into news features

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+// Project-level build.gradle.kts
+buildscript {
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.google.gms.google.services) apply false
-
 }
+
